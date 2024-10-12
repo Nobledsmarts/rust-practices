e0753b0869e8f1878581b1ddfb2a33c094d32953
nobledsmarts <nobledsmarts@gmail.com>
2024-10-12 14:25:27 +0100

    ownership and references

---
+++ ownership-references/target/debug/deps/ownership_references.d
@@ -0,0 +1,5 @@
+C:\rust-learning\ownership-references\target\debug\deps\ownership_references.exe: src/main.rs
+
+C:\rust-learning\ownership-references\target\debug\deps\ownership_references.d: src/main.rs
+
+src/main.rs:
---
+++ ownership-references/target/debug/ownership-references.d
@@ -0,0 +1,1 @@
+C:\rust-learning\ownership-references\target\debug\ownership-references.exe: C:\rust-learning\ownership-references\src\main.rs
